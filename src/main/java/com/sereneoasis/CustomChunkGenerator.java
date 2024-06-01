3609c98804a192085c560fd851e69ecb4ec88056
sereneoasis <sereneoasismc@gmail.com>
2024-06-01 12:50:11 +0100

    Separate Chunk Generation into multiple methods

--- src/main/java/com/sereneoasis/CustomChunkGenerator.java
+++ src/main/java/com/sereneoasis/CustomChunkGenerator.java
@@ -33,7 +33,7 @@ public class CustomChunkGenerator extends ChunkGenerator {
     }
 
     @Override
-    public void generateNoise(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {
+    public void generateNoise(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, ChunkData chunkData) {
         for(int y = chunkData.getMinHeight(); y < 130 && y < chunkData.getMaxHeight(); y++) {
             for(int x = 0; x < 16; x++) {
                 for(int z = 0; z < 16; z++) {
@@ -41,21 +41,13 @@ public class CustomChunkGenerator extends ChunkGenerator {
                     float noise3 = detailNoise.GetNoise(x + (chunkX * 16), y, z + (chunkZ * 16));
                     float currentY = (65 + (noise2 * 30));
 
-                    if(y < 1) {
-                        chunkData.setBlock(x, y, z, layers.get(3).get(random.nextInt(layers.get(3).size())));
-                    }
-                    else if(y < currentY) {
+                    if(y < currentY) {
                         float distanceToSurface = Math.abs(y - currentY); // The absolute y distance to the world surface.
                         double function = .1 * Math.pow(distanceToSurface, 2) - 1; // A second grade polynomial offset to the noise max and min (1, -1).
 
                         if(noise3 > Math.min(function, -.3)) {
-                            // Set grass if the block closest to the surface.
-                            if(distanceToSurface < 1 && y > 63) {
-                                chunkData.setBlock(x, y, z, layers.get(0).get(0));
-                            }
-
                             // It is not the closest block to the surface but still very close.
-                            else if(distanceToSurface < 5) {
+                            if(distanceToSurface < 5) {
                                 chunkData.setBlock(x, y, z, layers.get(1).get(random.nextInt(layers.get(1).size())));
                             }
 
@@ -88,6 +80,56 @@ public class CustomChunkGenerator extends ChunkGenerator {
                 }
             }
         }
+    }
+
+    @Override
+    public void generateSurface(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkGenerator.ChunkData chunkData) {
+        for(int y = chunkData.getMinHeight(); y < 130 && y < chunkData.getMaxHeight(); y++) {
+            for(int x = 0; x < 16; x++) {
+                for(int z = 0; z < 16; z++) {
+                    float noise2 = (terrainNoise.GetNoise(x + (chunkX * 16), z + (chunkZ * 16)) * 2) + (detailNoise.GetNoise(x + (chunkX * 16), z + (chunkZ * 16)) / 10);
+                    float noise3 = detailNoise.GetNoise(x + (chunkX * 16), y, z + (chunkZ * 16));
+                    float currentY = (65 + (noise2 * 30));
+
+                    if(y < 1) {
+                        chunkData.setBlock(x, y, z, layers.get(3).get(random.nextInt(layers.get(3).size())));
+                    }
+                    else if(y < currentY) {
+                        float distanceToSurface = Math.abs(y - currentY); // The absolute y distance to the world surface.
+                        double function = .1 * Math.pow(distanceToSurface, 2) - 1; // A second grade polynomial offset to the noise max and min (1, -1).
+
+                        if(noise3 > Math.min(function, -.3)) {
+                            // Set grass if the block closest to the surface.
+                            if(distanceToSurface < 1 && y > 63) {
+                                chunkData.setBlock(x, y, z, layers.get(0).get(0));
+                            }
+
+                        }
+                    }
+                    else if(y < 62) {
+                        chunkData.setBlock(x, y, z, Material.WATER);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void generateBedrock(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkGenerator.ChunkData chunkData) {
+        for(int y = chunkData.getMinHeight(); y < 130 && y < chunkData.getMaxHeight(); y++) {
+            for(int x = 0; x < 16; x++) {
+                for(int z = 0; z < 16; z++) {
+                    if(y < 1) {
+                        chunkData.setBlock(x, y, z, layers.get(3).get(random.nextInt(layers.get(3).size())));
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void generateCaves(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkGenerator.ChunkData chunkData) {
+        super.generateCaves(worldInfo, random, chunkX, chunkZ, chunkData);
     }
 
     @NotNull
