fc4597c7f9546d11c68839c520864498bfb947e4
Sujan Naik <sujannaik047@gmail.com>
2024-06-10 13:35:59 +0100

    Remove unused command class

--- src/main/java/com/sereneoasis/SereneRPG.java
+++ src/main/java/com/sereneoasis/SereneRPG.java
@@ -1,6 +1,5 @@
 package com.sereneoasis;
 
-import com.sereneoasis.command.SerenityCommand;
 import com.sereneoasis.config.FileManager;
 import com.sereneoasis.level.world.Schematics;
 import com.sereneoasis.level.world.biome.BiomeRepresentation;
@@ -10,7 +9,6 @@ import com.sereneoasis.level.world.noise.NoiseMaster;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
 import java.util.logging.Level;
 
 public class SereneRPG extends JavaPlugin {
@@ -23,10 +21,6 @@ public class SereneRPG extends JavaPlugin {
         return fileManager;
     }
 
-    public static void SereneRPG(String[] args) {
-        System.out.println("Hello world!");
-    }
-
 
     @Override
     public void onEnable() {
@@ -34,41 +28,10 @@ public class SereneRPG extends JavaPlugin {
         plugin = this;
         fileManager = new FileManager();
         new Schematics();
-//        CustomBiome.addCustomBiome(SereneBiomeData.PLAINS);
-//        CustomBiome.addCustomBiome(SereneBiomeData.DESERT);
-//        CustomBiome.addCustomBiome(SereneBiomeData.JUNGLE);
-//        CustomBiome.addCustomBiome(SereneBiomeData.OCEAN);
-//        CustomBiome.addCustomBiome(SereneBiomeData.RIVER);
-//        CustomBiome.addCustomBiome(SereneBiomeData.FOREST);
         this.getServer().getPluginManager().registerEvents(new SereneListener(), this);
-        this.getCommand("sereneRPG").setExecutor(new SerenityCommand());
         new DefaultFeatures();
 
-//        scheduleBiomeSwitching();
     }
-
-//    private Set<Chunk>changedChunks = new HashSet<>();
-//
-//    private void scheduleBiomeSwitching(){
-//        Bukkit.getServer().getScheduler().runTaskLater(this, () -> {
-//            World world = Bukkit.getWorld("test");
-//            Set<Chunk> newChunks = Arrays.stream(world.getLoadedChunks()).filter(chunk -> !changedChunks.contains(chunk)).collect(Collectors.toSet());
-//            newChunks.forEach(chunk -> {
-//                        ChunkSnapshot chunkSnapshot = chunk.getChunkSnapshot(false, true, false);
-//                        switch (chunkSnapshot.getBiome(8, 130, 8)) {
-//                            case PLAINS -> BiomeHelper.setCustomBiome("serene_oasis:plains", chunk);
-//                            case FOREST -> BiomeHelper.setCustomBiome("serene_oasis:forest", chunk);
-//                            case RIVER -> BiomeHelper.setCustomBiome("serene_oasis:river", chunk);
-//                            case JUNGLE -> BiomeHelper.setCustomBiome("serene_oasis:jungle", chunk);
-//                            case OCEAN -> BiomeHelper.setCustomBiome("serene_oasis:ocean", chunk);
-//                            case DESERT -> BiomeHelper.setCustomBiome("serene_oasis:desert", chunk);
-//                        }
-//                    });
-//            changedChunks.addAll(newChunks);
-//            Bukkit.broadcastMessage("Switched biomes");
-//            scheduleBiomeSwitching();
-//        }, 100L);
-//    }
 
     @Override
     public void onDisable() {
