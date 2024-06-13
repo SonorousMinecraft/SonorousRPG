bba0fde309a011c16dc89284a899dd9126965445
Sujan Naik <sujannaik047@gmail.com>
2024-06-13 17:21:47 +0100

    Implement Birch tree

--- src/main/java/com/sereneoasis/level/world/tree/TreeGenerationUtils.java
+++ src/main/java/com/sereneoasis/level/world/tree/TreeGenerationUtils.java
@@ -5,6 +5,7 @@ import com.sereneoasis.utils.BlockUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.data.type.Leaves;
 import org.bukkit.util.Vector;
 import oshi.util.tuples.Pair;
@@ -84,7 +85,6 @@ public class TreeGenerationUtils {
         FastNoiseLite noise = new FastNoiseLite(random.nextInt(100000));
         noise.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
         noise.SetFrequency(0.20F);
-//        noise.SetFractalOctaves(2);
         noise.SetFractalType(FastNoiseLite.FractalType.PingPong);
 
         for (int height = -iterations/6; height < iterations/2; height++) {
@@ -105,4 +105,55 @@ public class TreeGenerationUtils {
         trunk.forEach(location -> location.getBlock().setType(Material.ACACIA_LOG));
 
     }
+
+    public static void generateBirchTree(Location origin, int iterations, Random random ){
+        Location currentTrunkPos = origin.clone();
+
+        Set<Location> trunk = new HashSet<>();
+        Set<Pair<Vector, Location> >branches = new HashSet<>();
+
+        for (int i = 0; i < iterations ; i ++){
+
+            trunk.add(currentTrunkPos.clone().add(0,1,0));
+            trunk.add(currentTrunkPos.clone().add(-1,0,0));
+            trunk.add(currentTrunkPos.clone().add(1,0,0));
+            trunk.add(currentTrunkPos.clone().add(0,0,-1));
+            trunk.add(currentTrunkPos.clone().add(0,0,1));
+
+            currentTrunkPos.add(random.nextDouble() - 0.5,1,random.nextDouble() - 0.5);
+
+            Pair<Vector, Location> branch = new Pair<>(new Vector(random.nextDouble()-0.5, random.nextDouble()-0.5, random.nextDouble()-0.5).normalize(), currentTrunkPos.clone());
+            branches.add(branch);
+
+        }
+
+        FastNoiseLite noise = new FastNoiseLite(random.nextInt(100000));
+        noise.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
+        noise.SetFrequency(0.20F);
+        noise.SetFractalType(FastNoiseLite.FractalType.PingPong);
+
+        branches.forEach(vectorLocationPair -> {
+                    for (int i = 0; i < random.nextInt(1, iterations); i++) {
+                        vectorLocationPair.getB().add(vectorLocationPair.getA());
+                        vectorLocationPair.getB().getBlock().setType(Material.BIRCH_LOG);
+                        vectorLocationPair.getA().add(new Vector(0,0.2,0)).normalize();
+
+                    }
+                    BlockUtils.getAirBlocksAroundPoint(vectorLocationPair.getB(), 7 ).forEach(block -> {
+                        if (noise.GetNoise(block.getX(), block.getY(), block.getZ()) > 0.35) {
+                            block.setType(Material.BIRCH_LEAVES);
+                            Leaves blockData = (Leaves) block.getBlockData();
+                            blockData.setPersistent(true);
+                            block.setBlockData(blockData);
+
+                        }
+                    });
+
+                }
+            );
+
+        trunk.forEach(location -> location.getBlock().setType(Material.BIRCH_LOG));
+
+    }
+
 }
