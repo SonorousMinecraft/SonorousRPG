40a8b3a0bdef7032dae9d46e7fe4e28803803c64
Sujan Naik <sujannaik047@gmail.com>
2024-06-21 16:04:30 +0100

    Removal of video system

--- src/main/java/com/sereneoasis/command/SerenityCommand.java
+++ src/main/java/com/sereneoasis/command/SerenityCommand.java
@@ -1,46 +1,17 @@
 package com.sereneoasis.command;
 
-import com.sereneoasis.SereneRPG;
-import com.sereneoasis.chat.ChatConfiguration;
-import com.sereneoasis.chat.ChatManager;
-import com.sereneoasis.chat.ChatMaster;
-import com.sereneoasis.chat.builders.ChatBuilder;
 import com.sereneoasis.enchantments.EnchantmentWrapper;
 import com.sereneoasis.enchantments.Enchantments;
-import com.sereneoasis.entity.HumanEntity;
 import com.sereneoasis.items.ItemStacks;
 import com.sereneoasis.level.world.tree.TreeGenerationUtils;
-import com.sereneoasis.npc.types.NPCMaster;
-import com.sereneoasis.npc.types.assassin.AssassinEntity;
-import com.sereneoasis.utils.NPCUtils;
-import com.sereneoasis.video.MapRenderMan;
-import com.sereneoasis.video.MapStitcher;
-import com.sereneoasis.video.VideoFrameGrabber;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.*;
-import net.md_5.bungee.api.chat.hover.content.Item;
-import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.MapMeta;
-import org.bytedeco.javacv.FrameGrabber;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Random;
 
 public class SerenityCommand implements CommandExecutor {
 
@@ -88,108 +59,7 @@ public class SerenityCommand implements CommandExecutor {
 
                     }
                 }
-                case "video" -> {
-                    if (strings.length == 1){
-                        return false;
-                    }
-                    if (strings[1].equals("browse")){
-                        if (strings.length == 2){
-                            return false;
-                        }
-                        try {
-                            Document mainDoc = Jsoup.connect(strings[2]).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36").get();
-
-//                            Elements allLinks = new Elements();
-
-                            List<String> allLinks = new ArrayList<>();
-                            mainDoc.select("video").forEach(element -> {
-                             allLinks.add(element.attr("data-mp4").toString());
-                            });
-                            mainDoc.select("video").forEach(element -> {
-                                allLinks.add(element.attr("src").toString());
-                            });
-
-                            mainDoc.select("source").forEach(element -> {
-                                allLinks.add(element.attr("src").toString());
-                            });
-
-                            mainDoc.select("meta").forEach(element -> {
-                                if (element.attr("property").equals("og:image")) {
-                                    String mediaLink = element.attr("content").toString();
-                                    if (mediaLink.contains("gif")){
-                                        allLinks.add(mediaLink);
-                                    }
-
-
-                                }
-                            });
-
-//                            for (String link : allLinks) {
-                            String link = allLinks.get(new Random().nextInt(allLinks.size()));
-                                if (link.contains("https://")) {
-                                    Bukkit.broadcastMessage(link);
-
-                                    new VideoFrameGrabber(link);
-                                    break;
-//                                }
-                            }
-                        } catch (IOException e) {
-//                            throw new RuntimeException(e);
-                        } catch (FrameGrabber.Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    else {
-                        try {
-                            new VideoFrameGrabber(strings[1]);
-                        } catch (MalformedURLException e) {
-                            throw new RuntimeException(e);
-                        } catch (FrameGrabber.Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-//                }
             }
-            
-            
-            
-////            TreeGenerationUtils.generateCherryTree(player.getLocation(), iterations, new Random());
-//            NPCMaster npc = NPCUtils.spawnNPC(player.getLocation(), player, strings[0], strings[0]);
-////            SerenityEntities.getInstance().getNpcs().add(npc);
-//            SereneRPG.plugin.addNPC(npc);
-//
-//            player.sendMessage("command run");
-////            NPCUtils.updateEquipment(npc, player);
-
-//            if (strings.length ==  0) {
-//                new MapStitcher(player);
-//            } else {
-//                new MapStitcher(player, strings[0]);
-//            }
-
-//
-//            try {
-//                new VideoFrameGrabber(strings[0]);
-//            } catch (MalformedURLException e) {
-//                throw new RuntimeException(e);
-//            } catch (FrameGrabber.Exception e) {
-//                throw new RuntimeException(e);
-//            }
-
-//            if (strings.length == 0){
-//
-//
-//            } else {
-//                switch (strings[0]) {
-//                    case "next" -> {
-//                        ChatMaster.getInstance(player).next(player, Integer.valueOf(strings[1]));
-//                    }
-//                    case "stop" -> {
-//
-//                    }
-//                }
-//            }
-        }
         return true;
     }
 }
